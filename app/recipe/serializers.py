82826ead9507583ea057a3523840661f508aeec0
talfour <jak.malinowski94@gmail.com>
2021-06-07 20:28:27 +0100

    Added recipe image upload feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -8,8 +8,8 @@ class TagSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tag
-        fields = ('id', 'name')
-        read_only_fields = ('id',)
+        fields = ("id", "name")
+        read_only_fields = ("id",)
 
 
 class IngredientSerializer(serializers.ModelSerializer):
@@ -17,29 +17,45 @@ class IngredientSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ingredient
-        fields = ('id', 'name')
-        read_only_fields = ('id',)
+        fields = ("id", "name")
+        read_only_fields = ("id",)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serialize a recipe"""
+
     ingredients = serializers.PrimaryKeyRelatedField(
-        many=True,
-        queryset=Ingredient.objects.all()
+        many=True, queryset=Ingredient.objects.all()
     )
     tags = serializers.PrimaryKeyRelatedField(
-        many=True,
-        queryset=Tag.objects.all()
+        many=True, queryset=Tag.objects.all()
     )
 
     class Meta:
         model = Recipe
-        fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes',
-                  'price', 'link')
-        read_only_fields = ('id',)
+        fields = (
+            "id",
+            "title",
+            "ingredients",
+            "tags",
+            "time_minutes",
+            "price",
+            "link",
+        )
+        read_only_fields = ("id",)
 
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Serialize a recipe detail"""
+
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ("id", "image")
+        read_only_fields = ("id",)
